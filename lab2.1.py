6d49ddc517219ec28e537dbaa7a56327b9ff84ee
Finian Doonan <162761274+fin81985@users.noreply.github.com>
2025-02-08 11:45:50 +0000

    task2.1

---
+++ lab2.1.py
@@ -0,0 +1,20 @@
+print(2+4)
+print(2 + 4)
+#print(2 2+4)
+print(5*8)
+print(21*55)
+print(21/4)
+print( (int(21/4) ))
+print(21%4)
+print(2**8)
+print(2==3)
+print(2!=3) 
+print(3>3)
+print(3>=3)
+print(2+
+        4
+        )
+print(2+\
+        4
+        )
+
