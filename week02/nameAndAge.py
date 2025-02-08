87f7d956efa29803161e6b350c0c592974f53de6
Finian Doonan <162761274+fin81985@users.noreply.github.com>
2025-02-08 14:40:22 +0000

    moreex

---
+++ week02/nameAndAge.py
@@ -0,0 +1,24 @@
+# nameAndAge.py - This program asks the user for their name and age and then prints out a message addressed to them that tells them how old they will be in 10 years.
+# Author: Finian Doonan
+
+#name = input("Enter your name: ")
+#age = int(input("Enter your age: "))
+
+#print ( " Hello " + name + ", your age is "+ str(age) ) #coverting age to string to concatenate
+
+
+# name = input("Enter your name: ") #input function always returns a string
+# age = int(input("Enter your age: "))
+
+# print ( " Hello " + name + ",        your age is "+ str(age) )
+
+# input_number = int(input("Enter a number: "))#input function always returns a string
+# input_number2 = int(input("Enter a number: "))
+
+# print(input_number + input_number2)
+
+#if 2==3:
+  #  print("False")
+
+if 2!=3:
+    print("False")   
